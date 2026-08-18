6b263d58b2be797453881bf87192d757c88990a2
pasquale <estermileto@icloud.com>
2026-08-18 19:43:15 +0200

    Optimize local SEO for Calabria

--- server.js
+++ server.js
@@ -2195,6 +2195,7 @@ app.get('/sitemap.xml', async (req, res) => {
     { loc: 'https://www.automazionireitano.it/servizi/rifacimenti-revamping', priority: '0.8', changefreq: 'monthly' },
     { loc: 'https://www.automazionireitano.it/servizi/diagnostica-riparazioni', priority: '0.8', changefreq: 'monthly' },
     { loc: 'https://www.automazionireitano.it/servizi/automazione-industriale', priority: '0.8', changefreq: 'monthly' },
+    { loc: 'https://www.automazionireitano.it/automazione-industriale-reggio-calabria', priority: '0.8', changefreq: 'monthly' },
     ...projects.map((project) => ({
       loc: `https://www.automazionireitano.it/lavori/${encodeURIComponent(projectSlug(project))}`,
       priority: '0.7',
@@ -2241,6 +2242,10 @@ app.get('/servizi/diagnostica-riparazioni', (req, res) => {
 
 app.get('/servizi/automazione-industriale', (req, res) => {
   res.sendFile(path.join(PUBLIC_DIR, 'servizio-automazione-industriale.html'));
+});
+
+app.get('/automazione-industriale-reggio-calabria', (req, res) => {
+  res.sendFile(path.join(PUBLIC_DIR, 'automazione-industriale-reggio-calabria.html'));
 });
 
 app.get('/lavori', (req, res) => {
